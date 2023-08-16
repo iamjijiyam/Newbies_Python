409531eb705a00d39ae4b0fc6478a9d3c0e84318
jijiyam <malody17120@naver.com>
2023-08-16 09:45:59 +0900

    연산자 정리 코드 update

---
+++ 4강/5-3 연산자정리하기.py
@@ -0,0 +1,27 @@
+#연산자 정리 파일
+#증감 (a++,--b)
+#산술((),*,%,/,+,-)
+#시프트(<<,>>)
+#관계(>=,<=)
+#비트&&,II
+#논리
+#산술
+#삼항
+#대입
+
+a = 3
+b = 9
+
+plus = a + b
+minus = a - b
+multiply = a * b
+divide = a / b
+remainder = a % b
+
+print("합 = ",plus," 차 = ",minus,"곱 = ",multiply," 나누기 = ",divide," 나머지",remainder)
+
+
+
+
+
+
