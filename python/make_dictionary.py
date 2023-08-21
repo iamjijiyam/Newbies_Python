02593a8488e31e793b6f11f7daa8e0a7ce97245c
Jw <malody17120@naver.com>
2023-08-21 23:49:36 +0900

    아 미친 괄호 안씌워서 였다.. 젠장......... 조심하도록 하자

--- python/make_dictionary.py
+++ python/make_dictionary.py
@@ -1,12 +1,4 @@
-def lol_chams():
-    return "탑"
-
-def lol_jug(lol_chams):
-    return lol_chams()
-
 movie = {
-    "name": "비에고","릴리아","다리우스","사일러스" 
-    "type": "정글",lol_jug(lol_chams),"미드"  
-    }
-
-#자꾸 코드가 말을 안듣는다. 처치곤란. Dictionary entries must contain key/value pairs라고 뜨는데 난 쓰란대로 썻느데.............
+    "name": ["비에고", "릴리아", "다리우스", "사일러스"],
+    "type": ["정글", "탑", "미드"]
+}
