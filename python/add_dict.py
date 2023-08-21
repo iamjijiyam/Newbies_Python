677aa33fa2eabf8031b4ab6252388161b19c0f72
Jw <malody17120@naver.com>
2023-08-22 00:02:00 +0900

    주석 추가

---
+++ python/add_dict.py
@@ -0,0 +1,11 @@
+dictionary = {} #딕셔너리 선언해줌.
+
+print("before_add_component :",dictionary) # 현재는 아무것도 없는 딕셔너리를 추가해줍니다.
+
+dictionary["name"] = "이름을 새로 넣어주세요!" #딕셔너리에 요소를 추가해줬어요!
+dictionary["talk"] = "말을 시켜보아요!"
+dictionary["costume"] = "이쁜 옷을 입혀보아요!"
+
+print("after_add_component :",dictionary) #위의 여러 요소를 추가해줬어요! 이제 프린트를 통해 출력해봅시다!
+
+
