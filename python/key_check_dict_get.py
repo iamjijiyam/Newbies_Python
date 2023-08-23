c56c9a9ba498ae23bee973ce1addbee1cb7bed0a
Jw <malody17120@naver.com>
2023-08-23 23:47:18 +0900

    get.in 키워드 사용해서 딕셔너리에 키 있는지 확인해보기.

--- python/key_check_dict_get.py
+++ python/key_check_dict_get.py
@@ -1,1 +1,11 @@
+dictionary = {
+    "name" :"전문가용 연필",
+     "type": "필기구",
+      "ingredient" : ["흑연", "나무", "폴리에틸렌"],
+} # 딕셔너리 선언하기
 
+value = dictionary.get("존재하지 않는 키") #존재하지 않는 키에 접근
+print("값:",value)
+
+if value == None:
+    print("존재하지 않는 키에 접근했습니다. 데이터에선 None으로 표시됩니다.")
