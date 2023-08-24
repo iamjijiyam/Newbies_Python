211bcaca17ba06698b7fdaf7e0ac8894c005a967
Jw <malody17120@naver.com>
2023-08-24 23:43:47 +0900

    까먹고 안적은 한 문장!

--- python/key_check_dict_get.py
+++ python/key_check_dict_get.py
@@ -7,5 +7,5 @@ dictionary = {
 value = dictionary.get("존재하지 않는 키") #존재하지 않는 키에 접근
 print("값:",value)
 
-if value == None:
+if value == None: #None확인 시 결과 값이 None과 같은 지 확인만 해주면 돼요!
     print("존재하지 않는 키에 접근했습니다. 데이터에선 None으로 표시됩니다.")
